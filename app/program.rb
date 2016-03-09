2b68cb0f330f295ae2952632c2c200e1879feeec
Shintaro Kobori <shinkbr@gmail.com>
2016-03-10 08:51:18 +0900

    implement A and prime?

--- app/program.rb
+++ app/program.rb
@@ -1,10 +1,20 @@
+require 'prime'
+
 def A(x, y)
   # 関数Aを作成する．
   # 返り値は整数
+
+  if x == 0
+    return y + 1
+  elsif y == 0
+    return A(x - 1, 1)
+  else
+    return A(x - 1, A(x, y - 1))
+  end
 end
 
 def prime?(n)
   # 素数を判定する関数 prime? を作成する．
   # 返り値は true or false
-  return true
+  return n.prime?
 end
